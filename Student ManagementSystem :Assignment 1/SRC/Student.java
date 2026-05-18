3b7fc97e51c2598badbffae55e92b5de18e7dddf
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2026-05-18 23:33:35 +0530

    Create Student.java

---
+++ Student ManagementSystem :Assignment 1/SRC/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private String course;
+
+    // Constructor
+    public Student(int id, String name, String course) {
+
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    // Display Method
+    @Override
+    public String toString() {
+
+        return "ID: " + id +
+                ", Name: " + name +
+                ", Course: " + course;
+    }
+}
